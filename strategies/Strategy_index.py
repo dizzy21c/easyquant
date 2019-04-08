8fcaf07090dec101f27ec329199fc125a80915b0
zhangjianxun <dizzy21c@gmail.com>
2019-04-08 15:36:23 +0800

    update code

--- strategies/Strategy_index.py
+++ strategies/Strategy_index.py
@@ -20,6 +20,8 @@ class calcStrategy(Thread):
         # self._df = pd.DataFrame(list(data))
         self.rdata = chkv[2]
 
+        #{'name': '"', 'open': 11.19, 'close': 11.1, 'now': 11.47, 'high': 11.63, 'low': 11.19, 'buy': 11.46, 'sell': 11.47, 'turnover': 54845630, 'volume': 629822482.49, 'bid1_volume': 52700, 'bid1': 11.46, 'bid2_volume': 94600, 'bid2': 11.45, 'bid3_volume': 10900, 'bid3': 11.44, 'bid4_volume': 40700, 'bid4': 11.43, 'bid5_volume': 60500, 'bid5': 11.42, 'ask1_volume': 5200, 'ask1': 11.47, 'ask2_volume': 70600, 'ask2': 11.48, 'ask3_volume': 116300, 'ask3': 11.49, 'ask4_volume': 248000, 'ask4': 11.5, 'ask5_volume': 26200, 'ask5': 11.51, 'date': '2019-04-08', 'time': '14:12:33'}
+
 
         # self.redis = redis.Redis(host='localhost', port=6379, db=0)
 
