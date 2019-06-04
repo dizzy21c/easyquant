bcc8236cf7e18a2e6a790b5067254028219e1ed0
zhangjianxun <dizzy21c@gmail.com>
2019-06-04 09:51:00 +0800

    update stop

--- stop.sh
+++ stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ps -ef|grep mytest | grep -v grep | awk '{print $2}' | xargs kill
+ps -ef|grep mydata | grep -v grep | awk '{print $2}' | xargs kill
+ps -ef|grep mywatch | grep -v grep | awk '{print $2}' | xargs kill
 
