4481457187b44e46a9943ff8694d102a9ea15a99
zhangjianxun <dizzy21c@gmail.com>
2020-06-13 22:44:25 +0800

    code backup

--- test/stock_test.py
+++ test/stock_test.py
@@ -14,19 +14,19 @@ class strategy(QAStrategyStockBase):
 
             print('LONG price=%8.2f' % (bar['close']))
 
-            # if self.positions.volume_long == 0:
-            self.send_order('BUY', 'OPEN', price=bar['close'], volume=1)
+            if self.positions.volume_long == 0:
+                self.send_order('BUY', 'OPEN', price=bar['close'], volume=1)
 
-            # if self.positions.volume_short > 0:
-            #     self.send_order('BUY', 'CLOSE', price=bar['close'], volume=1)
+            if self.positions.volume_short > 0:
+                self.send_order('BUY', 'CLOSE', price=bar['close'], volume=1)
 
         else:
             print('SHORT price=%8.2f' % (bar['close']))
 
-            # if self.positions.volume_short == 0:
-            #     self.send_order('SELL', 'OPEN', price=bar['close'], volume=1)
-            # if self.positions.volume_long > 0:
-            # self.send_order('SELL', 'CLOSE', price=bar['close'], volume=1)
+            if self.positions.volume_short == 0:
+                self.send_order('SELL', 'OPEN', price=bar['close'], volume=1)
+            if self.positions.volume_long > 0:
+                self.send_order('SELL', 'CLOSE', price=bar['close'], volume=1)
 
     def ma(self,):
         return QA.QA_indicator_MA(self.market_data, 2, 5)
@@ -36,6 +36,7 @@ class strategy(QAStrategyStockBase):
 
 if __name__ == '__main__':
     s = strategy(code=['000001', '000002','600822','000859'], frequence='day', start='2020-01-01', end='2020-12-31', strategy_id='stock-test')
-    s.debug()
+    # s.debug()
+    s.run_backtest()
     # s.update_account()
   
