f15bb9d5d3cfaea514fe5c5ec9ee330a2063291d
dizzy21c@gmail.com <dizzy21c@gmail.com>
2020-06-14 22:46:29 +0800

    code backup

--- test/stock_test.py
+++ test/stock_test.py
@@ -7,29 +7,35 @@ class strategy(QAStrategyStockBase):
     def on_bar(self, bar):
         res = self.ma()
         # print(res.iloc[-1])
-        if np.isnan(res.MA2[-1]) or np.isnan(res.MA5[-1]):
-            return
+        # if np.isnan(res.MA2[-1]) or np.isnan(res.MA5[-1]):
+        #     return
+        code=bar.name[1]
+        # print(bar.name)
+        # print("code=%s, ma2=%6.2f, m5=%6.2f" % (code, res.MA2[-1], res.MA5[-1]))
+        # print(res)
+        # print(self.get_positions(code))
+        if res.MA5[-1] > res.MA30[-1]:
+        # if res.DIF[-1] > res.DEA[-1]:
 
-        if res.MA2[-1] > res.MA5[-1]:
+            # print('LONG price=%8.2f' % (bar['close']))
 
-            print('LONG price=%8.2f' % (bar['close']))
+            if self.get_positions(code).volume_long == 0:
+                self.send_order('BUY', 'OPEN', code=code, price=bar['close'], volume=100)
 
-            if self.positions.volume_long == 0:
-                self.send_order('BUY', 'OPEN', price=bar['close'], volume=1)
-
-            if self.positions.volume_short > 0:
-                self.send_order('BUY', 'CLOSE', price=bar['close'], volume=1)
+            # if self.positions.volume_short > 0:
+            #     self.send_order('BUY', 'CLOSE', code=code, price=bar['close'], volume=1)
 
         else:
-            print('SHORT price=%8.2f' % (bar['close']))
+            # print('SHORT price=%8.2f' % (bar['close']))
 
-            if self.positions.volume_short == 0:
-                self.send_order('SELL', 'OPEN', price=bar['close'], volume=1)
-            if self.positions.volume_long > 0:
-                self.send_order('SELL', 'CLOSE', price=bar['close'], volume=1)
+            # if self.acc.positions == {} or self.acc.positions.volume_short == 0:
+            #     self.send_order('SELL', 'OPEN', code=code, price=bar['close'], volume=1)
+            if self.get_positions(code).volume_long > 0:
+                self.send_order('SELL', 'CLOSE', code=code, price=bar['close'], volume=100)
 
     def ma(self,):
-        return QA.QA_indicator_MA(self.market_data, 2, 5)
+        return QA.QA_indicator_MA(self.market_data, 5, 30)
+        # return QA.QA_indicator_MACD(self.market_data)
 
     def risk_check(self):
         pass
