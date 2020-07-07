7fc11ec5ddf878f9ed3fe69c4a1c34bdcbc0c339
zhangjianxun <dizzy21c@gmail.com>
2020-07-07 20:07:36 +0800

    code backup

--- tdx/tdx_toptop_calc.py
+++ tdx/tdx_toptop_calc.py
@@ -16,10 +16,10 @@ from multiprocessing import Process, Pool, cpu_count, Manager
 
 mongo = MongoIo()
 
-data_buf1 = Manager().dict()
-data_buf2 = Manager().dict()
+databuf_mongo = Manager().dict()
+databuf_tdxfunc = Manager().dict()
 pool_size = cpu_count()
-print("pool size=%d" % pool_size)
+# print("pool size=%d" % pool_size)
 def tdx_base_func(data, code_list = None):
     """
     准备数据
@@ -79,6 +79,8 @@ def tdx_base_func(data, code_list = None):
     #     print(data.tail(22))
 
     return data
+def do_tdx_func(key):
+    databuf_tdxfunc[key] = tdx_func(databuf_mongo[key])
 
 def tdx_func(datam, code_list = None):
     """
@@ -101,60 +103,32 @@ def tdx_func(datam, code_list = None):
     print(end_t, 'tdx_func spent:{}'.format((end_t - start_t)))
     return dataR.sort_index()
 
-def tdx_func_mp(datam, code_list = None):
+def tdx_func_mp():
+    start_t = datetime.datetime.now()
+    print("begin-tdx_func:", start_t)
     pool = Pool(cpu_count())
+    for i in range(pool_size):
+        pool.apply_async(do_tdx_func, args=(i, ))
+
+    pool.close()
+    pool.join()
+
+    # todo begin
+    dataR = pd.DataFrame()
+    for i in range(pool_size):
+        if len(dataR) == 0:
+            dataR = databuf_tdxfunc[i]
+        else:
+            dataR = dataR.append(databuf_tdxfunc[i])
+        # print(len(dataR))
+    dataR.sort_index()
+    # todo end
 
 
-def pre_rsrs_data_v2_func(data, N=18, M=252):
-    """
-    准备数据
-    """
-    highs = data.high
+    end_t = datetime.datetime.now()
+    print(end_t, 'tdx_func spent:{}'.format((end_t - start_t)))
 
-    # 斜率
-    data['beta'] = 0
-    data['R2'] = 0
-    beta_rsquared = np.zeros((len(data), 2),)
-
-    for i in range(N - 1, len(highs) - 1):
-    #for i in range(len(highs))[N:]:
-        df_ne = data.iloc[i - N + 1:i + 1, :]
-        model = sml.ols(formula='high~low',data = df_ne)
-        result = model.fit()
-
-        # beta = low
-        beta_rsquared[i + 1, 0] = result.params[1]
-        beta_rsquared[i + 1, 1] = result.rsquared
-
-    data[['beta', 'R2']] = beta_rsquared
-
-    # 日收益率
-    data['ret'] = data.close.pct_change(1)
-    
-    # 标准分
-    #data['beta_norm'] = (data['beta'] - data.beta.rolling(M).mean().shift(1))
-    #/ data.beta.rolling(M).std().shift(1)
-    data['beta_norm'] = data['beta'].rolling(M).apply(lambda x:scs.zscore(x.values)[-1])
-
-    beta = data.columns.get_loc('beta')
-    beta_norm = data.columns.get_loc('beta_norm')
-    data.iloc[:min(M, len(highs)), beta_norm] = scs.zscore(data.iloc[:min(M, len(highs)), beta].values)
-    #beta_norm = data.columns.get_loc('beta_norm')
-    #beta = data.columns.get_loc('beta')
-    #for i in range(min(M, len(highs))):
-    # data.iat[i, beta_norm] = (data.iat[i, beta] - data.iloc[:i - 1,
-    # beta].mean()) / data.iloc[:i - 1, beta].std() if (data.iloc[:i - 1,
-    # beta].std() != 0) else np.nan
-
-    #data.iat[2, beta_norm] = 0
-    data['RSRS_R2'] = data.beta_norm * data.R2
-    data = data.fillna(0)
-    
-    # 右偏标准分
-    data['beta_right'] = data.RSRS_R2 * data.beta
-
-    return data
-
+    return dataR
 
 def buy_sell_fun(price, S1=1.0, S2=0.8):
     """
@@ -232,6 +206,14 @@ def buy_sell_fun_mp(datam, S1=1.0, S2=0.8):
     # result['nav'] = result['nav']  - len(datam.index.levels[1]) + 1
     return dataR2
 
+def do_get_data_mp(key, codelist, st_start):
+    mongo_mp = MongoIo()
+    start_t = datetime.datetime.now()
+    print("begin-get_data do_get_data_mp: key=%s, time=%s" %( key,  start_t))
+    databuf_mongo[key] = mongo_mp.get_stock_day(codelist, st_start=st_start)
+    end_t = datetime.datetime.now()
+    print(end_t, 'get_data do_get_data_mp spent:{}'.format((end_t - start_t)))
+
 def get_data(st_start):
     start_t = datetime.datetime.now()
     print("begin-get_data:", start_t)
@@ -260,7 +242,7 @@ def get_data(st_start):
     codelist = ["600109", "600551", "600697", "601066", "000732", "000905", "002827","600338","002049","300620"]
     # codelist = ['600380','600822']
 
-    code_file = "../config/stock3_list.json"
+    code_file = "../config/stock_list.json"
     codelist = []
     with open(code_file, 'r') as f:
         data = json.load(f)
@@ -268,8 +250,30 @@ def get_data(st_start):
             if len(d) > 6:
                 d = d[len(d) - 6:len(d)]
             codelist.append(d)
+    subcode_len = int(len(codelist) / pool_size)
+    code_dict = {}
+    pool = Pool(cpu_count())
+    for i in range(pool_size):
+        if i < pool_size - 1:
+            code_dict[str(i)] = codelist[i* subcode_len : (i+1) * subcode_len]
+        else:
+            code_dict[str(i)] = codelist[i * subcode_len:]
 
-    # print("code list", codelist)
+        pool.apply_async(do_get_data_mp, args=(i, code_dict[str(i)], st_start))
+
+    pool.close()
+    pool.join()
+
+    # # todo begin
+    # data_day = pd.DataFrame()
+    # for i in range(pool_size):
+    #     if len(data_day) == 0:
+    #         data_day = databuf_mongo[i]
+    #     else:
+    #         data_day = data_day.append(databuf_mongo[i])
+    #     # print(len(dataR))
+    # data_day.sort_index()
+    # # todo end
     # 获取股票中文名称，只是为了看得方便，交易策略并不需要股票中文名称
     # stock_names = QA.QA_fetch_stock_name(codelist)
     # codename = [stock_names.at[code] for code in codelist]
@@ -277,63 +281,26 @@ def get_data(st_start):
     # data_day = QA.QA_fetch_stock_day_adv(codelist,
     #                                     '2010-01-01',
     #                                     '{}'.format(datetime.date.today(),)).to_qfq()
+
     # st_start="2018-12-01"
-    data_day = mongo.get_stock_day(codelist, st_start=st_start)
+    # data_day = mongo.get_stock_day(codelist, st_start=st_start)
 
     end_t = datetime.datetime.now()
+    # print("data-total-len:", len(dataR))
     print(end_t, 'get_data spent:{}'.format((end_t - start_t)))
 
-    return data_day
+    # return data_day
 
 if __name__ == '__main__':
     start_t = datetime.datetime.now()
     print("begin-time:", start_t)
 
-    # # ETF/股票代码，如果选股以后：我们假设有这些代码
-    # codelist = ['512690', '510900', '513100', '510300',
-    #             '512980', '512170', '515000', '512800',
-    #             '159941', '159994', '515050', '159920',
-    #             '159952', '159987', '159805', '159997',
-    #             '159919',]
-    #
-    # codelist = ['510300']
-    #
-    # # 获取ETF/股票中文名称，只是为了看得方便，交易策略并不需要ETF/股票中文名称
-    # #stock_names = QA.QA_fetch_etf_name(codelist)
-    # #codename = [stock_names.at[code, 'name'] for code in codelist]
-    #
-    # ## 读取 ETF基金 日线，存在index_day中
-    # #data_day = QA.QA_fetch_index_day_adv(codelist,
-    # #    start='2010-01-01',
-    # #    end='{}'.format(datetime.date.today()))
-    #
-    # # codelist = ['600519']
-    # #codelist = ['600239']
-    # #codelist = ['600338']
-    # codelist = ['600095','600822','600183']
-    # codelist = ["600109", "600551", "600697", "601066", "000732", "000905", "002827","600338","002049","300620"]
-    # # codelist = ['600380','600822']
-    #
-    # code_file = "../config/stock3_list.json"
-    # codelist = []
-    # with open(code_file, 'r') as f:
-    #     data = json.load(f)
-    #     for d in data['code']:
-    #         if len(d) > 6:
-    #             d = d[len(d) - 6:len(d)]
-    #         codelist.append(d)
-    #
-    # # print("code list", codelist)
-    # # 获取股票中文名称，只是为了看得方便，交易策略并不需要股票中文名称
-    # # stock_names = QA.QA_fetch_stock_name(codelist)
-    # # codename = [stock_names.at[code] for code in codelist]
-    #
-    # # data_day = QA.QA_fetch_stock_day_adv(codelist,
-    # #                                     '2010-01-01',
-    # #                                     '{}'.format(datetime.date.today(),)).to_qfq()
-    st_start="2018-12-01"
-    data_day = get_data(st_start)
-    indices_rsrsT = tdx_func(data_day)
+    st_start="2019-12-01"
+    # data_day = get_data(st_start)
+    # print(data_day)
+    # indices_rsrsT = tdx_func(data_day)
+    get_data(st_start)
+    indices_rsrsT = tdx_func_mp()
     resultT = buy_sell_fun_mp(indices_rsrsT)
     num = resultT.flag.abs().sum() / 2
     nav = resultT.nav[resultT.shape[0] - 1]
