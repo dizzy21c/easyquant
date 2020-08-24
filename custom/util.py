7e2c2beded309ba5c9760382e76db4b42915f962
zhangjianxun <dizzy21c@gmail.com>
2020-08-24 15:26:36 +0800

    code backup

---
+++ custom/util.py
@@ -0,0 +1,81 @@
+
+import pandas as pd
+from easyquant.indicator.base import *
+
+def new_df(df_day, data, now_price):
+    code = data['code']
+    # now_vol = data['volume']
+    last_time = pd.to_datetime(data['datetime'][0:10])
+    # print("code=%s, data=%s" % (self.code, self._data['datetime']))
+    # df_day = data_buf_day[code]
+    # df_day.loc[last_time]=[0 for x in range(len(df_day.columns))]
+    df_day.loc[(last_time, code), 'open'] = data['open']
+    df_day.loc[(last_time, code), 'high'] = data['high']
+    df_day.loc[(last_time, code), 'low'] = data['low']
+    df_day.loc[(last_time, code), 'close'] = now_price
+    df_day.loc[(last_time, code), 'vol'] = data['volume']
+    df_day.loc[(last_time, code), 'amount'] = data['amount']
+    return df_day
+
+def tdx_func(data):
+    CLOSE = data.close
+    OPEN = data.open
+    C = data.close
+    H = data.high
+    O = data.open
+    # TDX-FUNC
+    # QQ := ABS(MA(C, 10) / MA(C, 20) - 1) < 0.01;
+    # DD := ABS(MA(C, 5) / MA(C, 10) - 1) < 0.01;
+    # QD := ABS(MA(C, 5) / MA(C, 20) - 1) < 0.01;
+    # DQ := MA(C, 5) > REF(MA(C, 5), 1) and QQ and DD and QD;
+    # QQ1 := (MA(C, 3) + MA(C, 6) + MA(C, 12) + MA(C, 24)) / 4;
+    # QQ2 := QQ1 + 6 * STD(QQ1, 11);
+    # QQ3 := QQ1 - 6 * STD(QQ1, 11);
+    # DD1 := MAX(MAX(MA(C, 5), MA(C, 10)), MAX(MA(C, 10), MA(C, 20)));
+    # DD2 := MIN(MIN(MA(C, 5), MA(C, 10)), MIN(MA(C, 10), MA(C, 20)));
+    # B: EVERY(OPEN > CLOSE, 3);
+    # B9 := "MACD.MACD" > 0;
+    # B1 := C / REF(C, 1) > 1.03;
+    # ZZ: O <= DD2 and C >= DD1 and REF(C < O, 1) and C > QQ2 and C > QQ1 and QQ1 > O and O / QQ3 < 1.005 and DQ;
+    # B2 := SMA(MAX(CLOSE - REF(C, 1), 0), 2, 1) * C * 102;
+    # B3 := SMA(ABS(CLOSE - REF(C, 1)), 2, 1) * C * 100;
+    # B4 := B2 / B3 * 100 < 10;
+    # B5 := B and B4;
+    # B6 := MA(C, 5) < REF(MA(C, 5), 1);
+    # B7 := REF(MA(C, 5), 4) > REF(MA(C, 5), 5);
+    # B8 := (H - C) / C * 100 < 1 and REF((O - C) / C * 100 > 1, 1) and KDJ.J > 25;
+    # 大黑马出笼: C > O and B1 and B6 and B7 and B8 and REF(B5, 1) and B9 or ZZ;
+    # python
+    QQ = ABS(MA(C, 10) / MA(C, 20) - 1) < 0.01
+    DD = ABS(MA(C, 5) / MA(C, 10) - 1) < 0.01
+    QD = ABS(MA(C, 5) / MA(C, 20) - 1) < 0.01
+    DQ = IFAND4(MA(C, 5) > REF(MA(C, 5), 1), QQ, DD, QD, True, False)
+    QQ1 = (MA(C, 3) + MA(C, 6) + MA(C, 12) + MA(C, 24)) / 4
+    QQ2 = QQ1 + 6 * STD(QQ1, 11)
+    QQ3 = QQ1 - 6 * STD(QQ1, 11)
+    DD1 = MAX(MAX(MA(C, 5), MA(C, 10)), MAX(MA(C, 10), MA(C, 20)))
+    DD2 = MIN(MIN(MA(C, 5), MA(C, 10)), MIN(MA(C, 10), MA(C, 20)))
+    # BT1=IFAND3(REF(OPEN,1)>REF(CLOSE,1),REF(OPEN,2)>REF(CLOSE,2),True,False)
+    # B = EVERY(OPEN > CLOSE, 3)
+    B = IFAND3(O > C, REF(OPEN, 1) > REF(CLOSE, 1), REF(OPEN, 2) > REF(CLOSE, 2), True, False)
+    # B9=MACD(C,12,26,9)
+    B9 = MACD(C).MACD > 0
+    B1 = C / REF(C, 1) > 1.03
+    # ZZ = O <= DD2 and C >= DD1 and REF(C < O, 1) and C > QQ2 and C > QQ1 and QQ1 > O and O / QQ3 < 1.005 and DQ
+    ZZ1 = IFAND6(O <= DD2, C >= DD1, REF(IF(C < O, 1, 0), 1) > 0, C > QQ2, C > QQ1, QQ1 > O, True, False)
+    ZZ = IFAND3(ZZ1, O / QQ3 < 1.005, DQ, True, False)
+    B2 = SMA(MAX(CLOSE - REF(C, 1), 0), 2, 1) * C * 102
+    B3 = SMA(ABS(CLOSE - REF(C, 1)), 2, 1) * C * 100
+    B4 = B2 / B3 * 100 < 10
+    # B5 = B and B4
+    B5 = IFAND(B, B4, True, False)
+    B6 = MA(C, 5) < REF(MA(C, 5), 1)
+    B7 = REF(MA(C, 5), 4) > REF(MA(C, 5), 5)
+    # B8 = (H - C) / C * 100 < 1 and REF((O - C) / C * 100 > 1, 1) and KDJ.J > 25
+    B81 = IFAND((H - C) / C * 100 < 1, REF(IF((O - C) / C * 100 > 1, 1, 0), 1), True, False)
+    B8 = IFAND(B81, KDJ(data).KDJ_J > 25, True, False)
+    HMTJ1 = IFAND5(C > O, B1, B6, B7, B8, True, False)
+    HMTJ2 = IFAND3(HMTJ1, REF(IF(B5, 1, 0), 1) > 0, B9, True, False)
+    # 大黑马出笼= C > O and B1 and B6 and B7 and B8 and REF(B5, 1) and B9 OR ZZ
+    大黑马出笼 = IFOR(HMTJ2, ZZ, True, False)
+    return 大黑马出笼
